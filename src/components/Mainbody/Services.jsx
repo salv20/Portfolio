78928fefec395ed1c405281728f7fb8039dad78e
Salvation Amoke <salvationamoke@gmail.com>
2023-12-26 19:07:21 +0100

    services

--- src/components/Mainbody/Services.jsx
+++ src/components/Mainbody/Services.jsx
@@ -2,7 +2,12 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { FaComputer, } from 'react-icons/fa6'
+const images = ['/web-dev.png', '/teach.png']
+const role = ['frontend developmet', 'coaching and mentorship']
+const tools = [
+    ['html/css', 'javascript', 'tailwind', 'react', 'supabase'],
+    ['html/css', 'tailwinsbsbbd', 'javascript', 'react', 'supabase']
+]
 const Services = () => {
     return (
         <section className=" bg-headerLightBg py-12 text-navText">
@@ -11,53 +16,65 @@ const Services = () => {
                 <h1 className=' text-lg sm:text-xl md:text-3xl tracking-wider'>services i offer to my clients: </h1>
                 <Swiper
                     slidesPerView={2}
-                    spaceBetween={30}
+                    spaceBetween={60}
                     pagination={{
                         clickable: true,
                     }}
                     modules={[Pagination]}
                     className="mySwiper hidden md:block"
                 >
-                    <SwiperSlide className='bg-headerDarkBg space-y-8 text-center py-8 swiped'>
-                        <img src="/web-dev.png" alt="" className='w-32 mx-auto' />
-                        <h1>frontend developmet</h1>
-                        <ol className=' space-y-2 text-lg text-gray-400'>
-                            <li>html/css</li>
-                            <li>javascript</li>
-                            <li>animation</li>
-                            <li>react</li>
-                            <li>supabase</li>
-                        </ol>
+                    {
+                        images.map((imgs, index) => (
+                            <SwiperSlide
+                                key={index}
+                                className='bg-headerDarkBg space-y-8 text-center py-8 shadow-lg rounded-xl swiped relative'
+                            >
+                                <div className="box1"></div>
+                                <img src={`${imgs}`} alt="" className='w-32 mx-auto' />
+                                <h1>{role[index]}</h1>
+                                <ol className=' space-y-2 text-lg text-gray-400'>
+                                    {
+                                        tools[index].map((tool, index) => (
+                                            <li key={index}>{tool}</li>
+                                        ))
+                                    }
+                                </ol>
+                                <div className="box2"></div>
+                            </SwiperSlide>
+                        ))
+                    }
+                </Swiper>
+                {/*  */}
 
-                    </SwiperSlide>
-                    <SwiperSlide>
-
-                        <article className=" bg-headerDarkBg w-full">
-                            <FaComputer />
-                            <h1>frontend developmet</h1>
-                            <ol>
-                                <li>html/css</li>
-                                <li>javascript</li>
-                                <li>animation</li>
-                                <li>react</li>
-                                <li>supabse</li>
-                            </ol>
-                        </article>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <article className=" bg-headerDarkBg w-full">
-                            <FaComputer />
-                            <h1>frontend developmet</h1>
-                            <ol>
-                                <li>html/css</li>
-                                <li>javascript</li>
-                                <li>animation</li>
-                                <li>react</li>
-                                <li>supabse</li>
-                            </ol>
-                        </article>
-                    </SwiperSlide>
-
+                <Swiper
+                    slidesPerView={1}
+                    spaceBetween={30}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper md:hidden"
+                >
+                    {
+                        images.map((imgs, index) => (
+                            <SwiperSlide
+                                key={index}
+                                className='bg-headerDarkBg space-y-8 text-center py-8 shadow-lg rounded-xl swiped relative'
+                            >
+                                <div className="box1"></div>
+                                <img src={`${imgs}`} alt="" className='w-32 mx-auto' />
+                                <h1>{role[index]}</h1>
+                                <ol className=' space-y-2 text-lg text-gray-400'>
+                                    {
+                                        tools[index].map((tool, index) => (
+                                            <li key={index}>{tool}</li>
+                                        ))
+                                    }
+                                </ol>
+                                <div className="box2"></div>
+                            </SwiperSlide>
+                        ))
+                    }
                 </Swiper>
             </div>
         </section>
