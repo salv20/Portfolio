7a75bf5b1ff9a0b5c3e2f541287c1e817049ce83
Salvation Amoke <salvationamoke@gmail.com>
2023-12-27 08:26:12 +0100

    service ui

--- src/components/Mainbody/Services.jsx
+++ src/components/Mainbody/Services.jsx
@@ -3,12 +3,12 @@ import 'swiper/css/pagination';
 import { Pagination, EffectCreative } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-const images = ['/web-dev.png', '/teach.png', '/web-dev.png']
+const images = ['/web-dev.png', '/teach.png', '/baas.png']
 const role = ['frontend developmet', 'coaching and mentorship', 'baas']
 const tools = [
     ['html/css', 'javascript', 'tailwind', 'react', 'supabase'],
     ['html/css', 'tailwind', 'javascript', 'react', 'supabase'],
-    ['supabase', 'Postgres database', 'user authentication', 'realtime syncing', ' AI toolkit']
+    ['supabase', 'Postgres database', 'user authentication', ' AI toolkit']
 ]
 const Services = () => {
     return (
@@ -41,12 +41,12 @@ const Services = () => {
                         images.map((imgs, index) => (
                             <SwiperSlide
                                 key={index}
-                                className='bg-headerDarkBg space-y-8 text-center py-8 shadow-lg rounded-xl swiped relative'
+                                className='bg-headerDarkBg space-y-6 text-center py-8 shadow-lg rounded-xl swiped relative'
                             >
                                 <div className="box1"></div>
                                 <img src={`${imgs}`} alt="" className='w-32 mx-auto' />
-                                <h1>{role[index]}</h1>
-                                <ol className=' space-y-2 text-lg text-gray-400'>
+                                <h1 className='uppercase'>{role[index]}</h1>
+                                <ol className='space-y-2 text-lg text-gray-400'>
                                     {
                                         tools[index].map((tool, index) => (
                                             <li key={index}>{tool}</li>
@@ -78,7 +78,7 @@ const Services = () => {
                             >
                                 <div className="box1"></div>
                                 <img src={`${imgs}`} alt="" className='w-32 mx-auto' />
-                                <h1>{role[index]}</h1>
+                                <h1 className='uppercase'>{role[index]}</h1>
                                 <ol className='space-y-2 text-lg text-gray-400'>
                                     {
                                         tools[index].map((tool, index) => (
@@ -107,11 +107,11 @@ const Services = () => {
                         images.map((imgs, index) => (
                             <SwiperSlide
                                 key={index}
-                                className='bg-headerDarkBg space-y-8 text-center py-8 shadow-lg rounded-xl swiped relative'
+                                className='bg-headerDarkBg space-y-6 text-center py-8 shadow-lg rounded-xl swiped relative'
                             >
                                 <div className="box1"></div>
                                 <img src={`${imgs}`} alt="" className='w-32 mx-auto' />
-                                <h1>{role[index]}</h1>
+                                <h1 className='uppercase'>{role[index]}</h1>
                                 <ol className=' space-y-2 text-lg text-gray-400'>
                                     {
                                         tools[index].map((tool, index) => (
