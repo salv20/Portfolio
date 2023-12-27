3fbe785bbe18bb1384380dd634bc8a7e0bb8aa68
Salvation Amoke <salvationamoke@gmail.com>
2023-12-27 21:37:56 +0100

    skills ui

--- src/components/Mainbody/Skills.jsx
+++ src/components/Mainbody/Skills.jsx
@@ -2,14 +2,14 @@
 const Skills = () => {
     return (
         <section className="bg-headerDarkBg text-white pt-4 pb-6 space-y-2">
-            <h1 className=" uppercase text-center">my skills</h1>
-            <div className="w-5/6 mx-auto grid grid-cols-3 text-center uppercase skill gap-4 bg-headerLightBg p-8">
-                <article className="">html</article>
-                <article className="">vanilla css</article>
-                <article className="">tailwind css</article>
-                <article className="">javascript</article>
-                <article className="">React js</article>
-                <article className="">supabase</article>
+            <h1 className=" uppercase text-center text-lg">my skills</h1>
+            <div className="w-5/6 mx-auto grid md:grid-cols-2 lg:grid-cols-3 text-center uppercase skill gap-4 bg-headerLightBg p-8">
+                <p>html</p>
+                <p>vanilla css</p>
+                <p>tailwind css</p>
+                <p>javascript</p>
+                <p>React js</p>
+                <p>supabase</p>
             </div>
         </section>
     )
