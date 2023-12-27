a04c9836825a5f9ee8cb5fa312b5ac3d1c7492bc
Salvation Amoke <salvationamoke@gmail.com>
2023-12-27 21:42:39 +0100

    .

--- src/components/Mainbody/Skills.jsx
+++ src/components/Mainbody/Skills.jsx
@@ -3,7 +3,7 @@ const Skills = () => {
     return (
         <section className="bg-headerDarkBg text-white pt-4 pb-6 space-y-2">
             <h1 className=" uppercase text-center text-lg">my skills</h1>
-            <div className="w-5/6 mx-auto grid md:grid-cols-2 lg:grid-cols-3 text-center uppercase skill gap-4 bg-headerLightBg p-8">
+            <div className="w-5/6 mx-auto grid md:grid-cols-2 lg:grid-cols-3 text-center uppercase skill gap-4 bg-headerLightBg p-4 sm:p-8">
                 <p>html</p>
                 <p>vanilla css</p>
                 <p>tailwind css</p>
