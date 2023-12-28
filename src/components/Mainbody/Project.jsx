308367b30ab06939deb4726eec8ca52d6affa36e
Salvation Amoke <salvationamoke@gmail.com>
2023-12-28 13:28:00 +0100

    slide per view

--- src/components/Mainbody/Project.jsx
+++ src/components/Mainbody/Project.jsx
@@ -5,14 +5,14 @@ const name = ['glints', 'store', 'travel-app']
 import { FaGithub } from "react-icons/fa"
 const Project = () => {
     return (
-        <section className=' bg-headerDarkBg py-8 service'>
+        <section className=' bg-headerDarkBg py-8'>
             <div className=" w-5/6 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 text-center gap-4">
                 {
                     images.map((img, index) => (
 
-                        <div className=" bg-navText rounded-2xl" key={index}>
+                        <div className=" bg-navText rounded-xl" key={index}>
                             <a href={`${liveWeb[index]}`} >
-                                <img src={`${img}`} alt="" className=" rounded-t-xl" />
+                                <img src={`${img}`} alt="" className=" rounded-t-lg" />
                             </a>
                             <div className=" text-center uppercase space-y-1 py-2">
                                 <p>{name[index]}</p>
--- src/components/Mainbody/Skills.jsx
+++ src/components/Mainbody/Skills.jsx
@@ -5,43 +5,9 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 const skillList = ['html/css', 'tailwind css', 'javascript', 'react js', 'supabase', 'communication']
 const Skills = () => {
     return (
-        <section className="bg-headerDarkBg text-white pt-4 pb-6 space-y-2">
+        <section className="bg-headerDarkBg text-white pt-4 pb-8 space-y-2">
             <h1 className=" uppercase text-center text-lg">my skills</h1>
             <div className="w-5/6 mx-auto bg-headerLightBg ">
-
-                {/* THREE SLIDES PER VIEW */}
-                <Swiper
-                    slidesPerView={3}
-                    spaceBetween={20}
-                    pagination={{
-                        clickable: true,
-                    }}
-                    effect={'creative'}
-                    creativeEffect={{
-                        prev: {
-                            shadow: true,
-                            translate: [0, 0, -500],
-                        },
-                        next: {
-                            shadow: true,
-                            translate: ['100%', 0, 0],
-                        },
-                    }}
-                    modules={[Pagination, EffectCreative]}
-                    className="hidden sm:block text-center uppercase bg-headerDarkBg"
-                >
-                    {
-                        skillList.map((skill, index) => (
-                            <SwiperSlide
-                                key={index}
-                                className=' bg-headerLightBg text-center py-20 rounded-xl lg:py-24'
-
-                            >
-                                <p>{skill}</p>
-                            </SwiperSlide>
-                        ))
-                    }
-                </Swiper>
 
                 {/*SLIDE PER VIEW */}
                 <Swiper
@@ -62,7 +28,7 @@ const Skills = () => {
                         },
                     }}
                     modules={[Pagination, EffectCreative]}
-                    className="sm:hidden text-center uppercase bg-headerDarkBg"
+                    className="sm:hidden uppercase bg-headerDarkBg"
                 >
                     {
                         skillList.map((skill, index) => (
@@ -76,6 +42,32 @@ const Skills = () => {
                         ))
                     }
                 </Swiper>
+
+
+                {/* THREE SLIDES PER VIEW */}
+                <Swiper
+                    slidesPerView={3}
+                    spaceBetween={20}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    effect={'fade'}
+                    modules={[Pagination]}
+                    className="hidden sm:block uppercase bg-headerDarkBg"
+                >
+                    {
+                        skillList.map((skill, index) => (
+                            <SwiperSlide
+                                key={index}
+                                className=' bg-headerLightBg text-center py-20 rounded-xl lg:py-24'
+
+                            >
+                                <p>{skill}</p>
+                            </SwiperSlide>
+                        ))
+                    }
+                </Swiper>
+
             </div>
         </section>
     )
